814c3affb9bb29c708442718fb9d701f8b74fc2a
m4cd4r4 <m4cd4r4@gmail.com>
2024-08-06 15:06:30 +0800

    Output to single CSV with label & timestamp

---
+++ installed_apps_on_remote_machine_(outputs_a_labelled_timestamped_csv).ps1
@@ -0,0 +1,13 @@
+﻿$computerName = "SAN122248"
+$timestamp = Get-Date -Format "yyyyMMddHHmmss"
+$folderPath = "C:\Users\omuma\Desktop\Installed Software"
+$csvFileName = "$folderPath\" + $computerName + "_" + $timestamp + ".csv"
+
+# Check if the 'Installed Software' folder exists, if not, create it
+if (-not (Test-Path -Path $folderPath)) {
+    New-Item -ItemType Directory -Path $folderPath
+}
+
+Get-WmiObject -Class Win32_Product -ComputerName $computerName | 
+Select-Object Name, Version, Vendor, @{Name="Installed On"; Expression={$_.ConvertToDateTime($_.InstallDate)}} | 
+Export-Csv -Path $csvFileName -NoTypeInformation
