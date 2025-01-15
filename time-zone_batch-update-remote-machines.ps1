a2604f8d0c88c1a624d1752ad2cbe8363eb7fe86
m4cd4r4 <m4cd4r4@gmail.com>
2025-01-15 09:58:21 +0800

    Time-Zone - Update time-zone of remote machines
    Use the following cmdlet to get the Microsoft-supported Time Zone IDs
    
    Get-TimeZone -ListAvailable

---
+++ time-zone_batch-update-remote-machines.ps1
@@ -0,0 +1,33 @@
+﻿#Update time-zone of remote machines
+
+
+# List of remote computer names or IP addresses
+$computers = @("SAN122602")  # Add more computer names or IPs, separated by commas
+
+# Dili Time Zone ID is "Tokyo Standard Time"
+# WA Time Zone ID is "W. Australia Standard Time"
+$newTimeZone = "W. Australia Standard Time"
+
+# Define the script block that will change the time zone on the remote machine
+$scriptBlock = {
+    param($newTimeZone)
+    
+    # List all available time zones to check if the given time zone is valid
+    $availableTimeZones = Get-TimeZone -ListAvailable
+    $timezone = $availableTimeZones | Where-Object { $_.Id -eq $newTimeZone }
+    
+    if ($timezone) {
+        Set-TimeZone -Id $newTimeZone
+        Write-Host "Time zone updated to: $newTimeZone"
+    } else {
+        Write-Host "Invalid time zone: $newTimeZone"
+    }
+}
+
+# Run the script on each remote computer
+foreach ($computer in $computers) {
+    Write-Host "Updating time zone on $computer"
+    
+    # Use Invoke-Command to run the script on the remote machine
+    Invoke-Command -ComputerName $computer -ScriptBlock $scriptBlock -ArgumentList $newTimeZone -Credential (Get-Credential)
+}
